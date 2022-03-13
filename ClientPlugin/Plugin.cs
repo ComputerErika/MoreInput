9fa12707b3d248e34324f21296544f0596b69cb2
ebrom <ebromstead@gmail.com>
2022-03-13 05:21:59 -0700

    Inital Try

--- ClientPlugin/Plugin.cs
+++ ClientPlugin/Plugin.cs
@@ -9,13 +9,19 @@ using Shared.Patches;
 using Shared.Plugin;
 using VRage.FileSystem;
 using VRage.Plugins;
+// Non Base Plugin Libs
+using Linearstar.Windows.RawInput;
+using System.Windows.Forms;
+using VRageMath;
+using Sandbox.Game.World;
+using System.Linq;
 
-namespace ClientPlugin
+namespace MoreInput
 {
     // ReSharper disable once UnusedType.Global
     public class Plugin : IPlugin, ICommonPlugin
     {
-        public const string Name = "PluginTemplate";
+        public const string Name = "MoreInput";
         public static Plugin Instance { get; private set; }
 
         public long Tick { get; private set; }
@@ -29,6 +35,11 @@ namespace ClientPlugin
 
         private static bool initialized;
         private static bool failed;
+
+        // My Shit
+        RawInputReceiverWindow rirwindow;
+        Vector3 MovInput;
+        Vector3 RotInput;
 
         [System.Runtime.CompilerServices.MethodImpl(System.Runtime.CompilerServices.MethodImplOptions.NoInlining)]
         public void Init(object gameInstance)
@@ -55,6 +66,7 @@ namespace ClientPlugin
         {
             try
             {
+                RawInputDevice.UnregisterDevice(new HidUsageAndPage(0x01, 0x08));
                 // TODO: Save state and close resources here, called when the game exists (not guaranteed!)
                 // IMPORTANT: Do NOT call harmony.UnpatchAll() here! It may break other plugins.
             }
@@ -107,12 +119,55 @@ namespace ClientPlugin
 
         private void Initialize()
         {
-            // TODO: Put your one time initialization code here. It is executed on first update, not on loading the plugin.
+            rirwindow = new RawInputReceiverWindow();
+
+            rirwindow.Input += (sender, e) =>
+            {
+                byte[] data = e.Data.ToStructure();
+                int inputType = data.Skip(32).First();
+                byte[] inputData = data.Skip(33).Take(6).ToArray();
+                
+                if (inputType == 1)
+                {
+                    MovInput = new Vector3(
+                        -(((sbyte)inputData[0 + 1] << 8) + inputData[0]),
+                        -(((sbyte)inputData[0 + 1] << 8) + inputData[1]),
+                        -(((sbyte)inputData[2 + 1] << 8) + inputData[2])
+                        );
+                    MovInput /= 350; //Max 350
+                }
+
+                if (inputType == 2)
+                {
+                    RotInput = new Vector3(
+                        -(((sbyte)inputData[0 + 1] << 8) + inputData[0]),
+                        -(((sbyte)inputData[1 + 1] << 8) + inputData[1]),
+                        -(((sbyte)inputData[2 + 1] << 8) + inputData[2])
+                        );
+                    RotInput /= 350;
+                }
+
+                //if (inputType == 3) { keys = inputData[0]; }
+            };
+
+            RawInputDevice.RegisterDevice(new HidUsageAndPage(0x01, 0x08), RawInputDeviceFlags.ExInputSink, rirwindow.Handle);
+            //Application.Run();
         }
 
         private void CustomUpdate()
         {
-            // TODO: Put your update code here. It is called on every simulation frame!
+            if (MovInput == Vector3.Zero && RotInput == Vector3.Zero)
+            {
+                MySession.Static.ControlledEntity?.MoveAndRotateStopped();
+            }
+            else
+            {
+                //MovInput = Vector3.Clamp(MovInput, -Vector3.One, Vector3.One);
+                Vector2 rotatePY = new Vector2(RotInput.X, RotInput.Z);
+                float rotateR = RotInput.Y;
+                MySession.Static.ControlledEntity?.MoveAndRotate(MovInput, rotatePY, rotateR);
+            }
+
         }
 
 
@@ -120,6 +175,47 @@ namespace ClientPlugin
         public void OpenConfigDialog()
         {
             MyGuiSandbox.AddScreen(new MyPluginConfigDialog());
+        }
+    }
+
+    class RawInputEventArgs : EventArgs
+    {
+        public RawInputEventArgs(RawInputData data)
+        {
+            Data = data;
+        }
+
+        public RawInputData Data { get; }
+    }
+
+    class RawInputReceiverWindow : NativeWindow
+    {
+        public event EventHandler<RawInputEventArgs> Input;
+
+        public RawInputReceiverWindow()
+        {
+            CreateHandle(new CreateParams
+            {
+                X = 0,
+                Y = 0,
+                Width = 0,
+                Height = 0,
+                Style = 0x800000,
+            });
+        }
+
+        protected override void WndProc(ref Message m)
+        {
+            const int WM_INPUT = 0x00FF;
+
+            if (m.Msg == WM_INPUT)
+            {
+                var data = RawInputData.FromHandle(m.LParam);
+
+                Input?.Invoke(this, new RawInputEventArgs(data));
+            }
+
+            base.WndProc(ref m);
         }
     }
 }
